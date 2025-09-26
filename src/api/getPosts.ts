de274f485d19cdf8617bc48026c7505c157f7b33
solanariums <kistka.solomia@gmail.com>
2025-09-27 02:33:10 +0300

    get Posts

--- src/api/getPosts.ts
+++ src/api/getPosts.ts
@@ -5,7 +5,6 @@ export const getPosts = async (): Promise<Array<Post>> => {
   const response = await fetch('http://localhost:3002/api/posts', {
     headers: {
       Authorization: `Bearer ${localStorage.getItem('token')}`,
-      'Content-Type': 'multipart/form-data',
     },
   })
 
